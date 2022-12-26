4b5d6030f6ed98fee853d5c055653e51083d0631
ReFlik <brb271828@gmail.com>
2022-12-27 00:41:49 +0500

    fix all bugs

--- ClientApp/src/components/TableRow.js
+++ ClientApp/src/components/TableRow.js
@@ -1,110 +1,130 @@
-import {useState} from 'react'
+import React, { Component } from 'react';
 
-const TableRow = (props)=>{		
-	const [book, setBook] = useState(Object.assign({}, props.book));	
-	const [prevBook, setPrevBook] = useState(Object.assign({}, props.book));			
-	const [editing, setEditing] = useState(props.edit);	
-			
-	const addHandler = () => {
-		
-	};
-		
-	const changeHandler = () => {	
-		if( window.confirm("сохранить изменения?") )
-		{	
-			fetch('books/change',
-			{
-				method: 'POST',
-                headers: new Headers({ 'Content-Type': 'application/json' }),
-                body: JSON.stringify(book)
-			}).then(()=>{			
-				setPrevBook(Object.assign({}, book));		
-				setEditing(false);
-			});
-		}
-	};
-	
-	const canselHandler = () => {
-		setBook(Object.assign({}, prevBook));
-		setEditing(false);
-	};	
-	
-	const deleteHandler = (btn, id) => {
-		var row = btn.parentNode.parentNode;
-		
-		if( window.confirm("вы действительно хотите удалить книгу?") )
-		{	
-			fetch('books/delete',
-			{
-				method: 'POST',
-                headers: new Headers({ 'Content-Type': 'application/json' }),
-                body: JSON.stringify({ 
-					"id": book.id
-				})
-			})			
-			.then(()=>{ row.parentNode.removeChild(row); })
-            .then(data => console.log(data)) 
-		}
-	};
-		
-	const setProp = (name, target) => {
-		book[name] = target.value;
-		setBook(Object.assign({}, book));
-	};
-		
-	return (
-			<>
-			{	
-				editing 
-				? (		
-					<tr key={book.id}>
-						<td>{book.id}</td>
-						<td><input type="text" value={book.name} onChange={({ target }) => setProp("name", target)} /></td>
-						<td><input type="text" value={book.year} onChange={({ target }) => setProp("year", target)} /></td>
-						<td><input type="text" value={book.genre} onChange={({ target }) => setProp("genre", target)} /></td>
-						<td><input type="text" value={book.autor} onChange={({ target }) => setProp("autor", target) } /></td>
-						<td>
-							<button 
-								style={{ border:0, background:0, paddingLeft: "30px" }}
-								onClick={e => changeHandler()}
-							>
-								✓
-							</button>
-							<button 
-								style={{ border:0, background:0, paddingLeft: "30px", transform: "scale(-1, 1)" }}
-								onClick={e => canselHandler()}
-							> 
-								✖
-							</button>
-						</td>
-					</tr>	
-				) 				
-				: (	
-					<tr key={book.id}>
-						<td>{book.id}</td>
-						<td>{book.name}</td>
-						<td>{book.year}</td>
-						<td>{book.genre}</td>
-						<td>{book.autor}</td>
-						<td>
-							<button 
-								style={{ border:0, background:0, paddingRight: "30px", transform: "scale(-1, 1)" }}
-								onClick={e => setEditing(true)}
-							> 
-								✎
-							</button>
-							<button 
-								style={{ border:0, background:0, paddingLeft: "30px" }}
-								onClick={e => deleteHandler(e.target, book.id) }
-							>
-								🗑️
-							</button>
-						</td>
-					</tr>					
-				)			
-			}
-			</>
-	);	
-};
+class TableRow extends React.Component {
 
-export default TableRow;
+    constructor(props) {
+        super(props);      
+        this.deleteClick = this.deleteClick.bind(this);
+        this.state = { 
+            book: props.book, 
+            prevBook: props.book, 
+            editing: props.edit 
+        };
+    }
+  
+    setChange(){
+        this.setState(prevState=>({
+            book:  Object.assign({}, prevState.book),
+            prevBook: Object.assign({}, prevState.book),
+            editing: true
+        }));        
+    }
+    
+    changeHandler(){
+        if( window.confirm("сохранить изменения?") )
+        {
+            fetch('books/change',
+                {
+                    method: 'POST',
+                    headers: new Headers({ 'Content-Type': 'application/json' }),
+                    body: JSON.stringify(this.state.book)
+                }).then(()=>{
+                    this.setState(prevState=>({
+                        book: prevState.book,
+                        prevBook: prevState.prevBook,    
+                        editing: false
+                    }));
+            });
+        }    
+    }
+    
+    canselHandler(){
+        this.setState(prevState=>({
+            book: Object.assign({}, prevState.prevBook),
+            prevBook: prevState.prevBook,
+            editing: false
+        }));
+    }
+
+    setProp(name, target){
+        this.setState(prevState=> {
+            prevState.book[name] = target.value;
+            this.deleteClick = this.deleteClick.bind(this);
+            return {
+                book: prevState.book,
+                prevBook: prevState.prevBook,
+                editing: prevState.editing
+            }  
+        })
+    }
+
+    deleteClick(e){   
+        this.props.deleteClick(this.state.book.id, e);
+    }
+    
+    render(){
+        return (          
+            <>
+            {
+            this.state.editing
+            ? (
+                <tr key={this.state.book.id}>
+                    <td>{this.state.book.id}</td>
+                    <td><input type="text" value={this.state.book.name} 
+                        onChange={({ target }) => this.setProp("name", target)} 
+                        /></td>
+                    <td><input type="text" value={this.state.book.year} 
+                        onChange={({ target }) => this.setProp("year", target)}
+                        /></td>
+                    <td><input type="text" value={this.state.book.genre} 
+                        onChange={({ target }) => this.setProp("genre", target)} 
+                         /></td>
+                    <td><input type="text" value={this.state.book.autor} 
+                        onChange={({ target }) => this.setProp("autor", target) } 
+                        /></td>
+                    <td>
+                        <button
+                            style={{ border:0, background:0, paddingLeft: "30px" }}
+                            onClick={e => this.changeHandler()}
+                        >
+                            ✓
+                        </button>
+                        <button
+                            style={{ border:0, background:0, paddingLeft: "30px", transform: "scale(-1, 1)" }}
+                            onClick={()=>this.canselHandler()}
+                        >
+                            ✖
+                        </button>
+                    </td>
+                </tr>
+            )
+            : (
+            <tr key={this.state.book.id}>
+                <td>{this.state.book.id}</td>
+                <td>{this.state.book.name}</td>
+                <td>{this.state.book.year}</td>
+                <td>{this.state.book.genre}</td>
+                <td>{this.state.book.autor}</td>
+                <td>
+                    <button
+                        style={{ border:0, background:0, paddingRight: "30px", transform: "scale(-1, 1)" }}
+                        onClick={()=>this.setChange()}
+                    >
+                        ✎
+                    </button>
+                    <button
+                        style={{ border:0, background:0, paddingLeft: "30px" }}
+                        onClick={(e)=>this.deleteClick(e)}
+                    >
+                        🗑️
+                    </button>
+                </td>
+            </tr>
+            )
+            }
+            </>
+        )
+    }
+}
+
+export default (TableRow);
